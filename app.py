6975e3d25a4968fea287a36e6b184ad192df4fa7
Avinash Galande <avinashgalande96@gmail.com>
2022-09-15 14:58:30 +0530

    changed port and host name for flask app

--- app.py
+++ app.py
@@ -43,5 +43,7 @@ def predict():
     return render_template('index.html', pred=result )
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host ="0.0.0.0",port = 8080)
+
 
